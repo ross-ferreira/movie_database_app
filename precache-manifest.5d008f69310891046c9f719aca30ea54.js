12d4579e4899cac578f28d17449009086d5dadb5
Ross Ferreira <ross.p.ferreira@gmail.com>
2020-05-11 09:15:16 +0100

    Updates

---
+++ precache-manifest.5d008f69310891046c9f719aca30ea54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1689163771555f136fb89019b117948d",
+    "url": "/movie_database_app/index.html"
+  },
+  {
+    "revision": "5fa58a80847f184d5ab8",
+    "url": "/movie_database_app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4964b773df23044a1330",
+    "url": "/movie_database_app/static/js/2.6bf24dcd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_database_app/static/js/2.6bf24dcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa58a80847f184d5ab8",
+    "url": "/movie_database_app/static/js/main.080e343d.chunk.js"
+  },
+  {
+    "revision": "124d0a4fefaebe9f5d7f",
+    "url": "/movie_database_app/static/js/runtime-main.d2ea8187.js"
+  }
+]);
